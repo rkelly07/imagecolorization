e30dce0029d939046d6b65732f781cfd1b0a5892
Michael DeLaus <mdelaus@mit.edu>
2016-12-11 14:20:40 -0500

    removed down and upsampling

--- colorization_nn.py
+++ colorization_nn.py
@@ -1,6 +1,7 @@
 #Attempt at conv nn as defined by http://richzhang.github.io/colorization/
 from tflearn import *
 import numpy as np
+import tensorflow as tf
 
 X = np.load('flowersbw.npy')
 Y = np.load('flowers.npy')
@@ -8,21 +9,22 @@ Y = np.load('flowers.npy')
 #network = dropout(network, 0.5)
 
 # Building deep neural network, input is black and white photo
-input_layer = input_data(shape=[None, 256, 256, 1])
+input_layer = input_data(shape=[None, 256, 256])
+input_layer = tf.reshape(input_layer, [-1,256,256,1])
 
 #conv1 256x256x1
 network = conv_2d(input_layer, 64, 3, activation='relu')
-network = conv_2d(network, 64, 3, activation='relu', strides=2)
+network = conv_2d(network, 64, 3, activation='relu')
 network = batch_normalization(network)
 
 #conv2 128x128x64
 network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d(network, 128, 3, activation='relu', strides=2)
+network = conv_2d(network, 128, 3, activation='relu')
 network = batch_normalization(network)
 
 #conv3 64x64x128
 network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu', strides=2)
+network = conv_2d(network, 256, 3, activation='relu')
 network = batch_normalization(network)
 
 #conv4 32x32x256
@@ -42,21 +44,25 @@ network = batch_normalization(network)
 
 #conv7 32x32x512
 network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 3, 3, activation='relu')
 network = batch_normalization(network)
-
+'''
 #conv8 32x32x512
 network = conv_2d(network, 256, 3, activation='relu')
 network = conv_2d_transpose(network, 256 , 3, [64,64,256], activation='relu')
 
 #conv8 32x32x512
 network = conv_2d(network, 128, 3, activation='relu')
+print network.get_shape()
 network = conv_2d_transpose(network, 128 , 3, [128,128,128], activation='relu')
+print network.get_shape()
 
 #conv8 32x32x512
 network = conv_2d(network, 64, 3, activation='relu')
+print network.get_shape()
 network = conv_2d_transpose(network, 3 , 3, [256,256,3], activation='relu')
-
+print network.get_shape()
+'''
 loss = losses.L2(Y-network)
 
 network = regression(network, optimizer='Adam',
@@ -68,7 +74,7 @@ model = DNN(network, checkpoint_path='saveFiles/colorizer',
 
 #model.load('what I called it')
 
-model.fit(X, Y, n_epoch=1000, validation_set=0.1, shuffle=True,
+model.fit(X, Y, n_epoch=10, validation_set=0.1, shuffle=True,
           show_metric=True, batch_size=32, snapshot_step=1000,
           snapshot_epoch=False, run_id='flowers')
 
