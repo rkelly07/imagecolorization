be3f6f7ae8077685a18f52e3a7f90247252eeebc
Michael DeLaus <mdelaus@mit.edu>
2016-12-11 15:14:00 -0500

    honestly idk

--- colorization_nn.py
+++ colorization_nn.py
@@ -14,56 +14,55 @@ input_layer = input_data(shape=[None, 256, 256])
 input_layer = tf.reshape(input_layer, [-1,256,256,1])
 
 #conv1 256x256x1
-network = conv_2d(input_layer, 64, 3, activation='relu')
+network = conv_2d(input_layer, 64, 3, activation='relu', name='Conv1_1')
 
-network = conv_2d(network, 64, 3, activation='relu', strides=2)
+network = conv_2d(network, 64, 3, activation='relu', strides=2, name='Conv1_2')
 network = batch_normalization(network)
 
 #conv2 128x128x64
-network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d(network, 128, 3, activation='relu', strides=2)
+network = conv_2d(network, 128, 3, activation='relu',name='Conv2_1')
+network = conv_2d(network, 128, 3, activation='relu', strides=2, name='Conv2_2')
 network = batch_normalization(network)
 
 #conv3 64x64x128
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d(network, 256, 3, activation='relu', strides=2)
+network = conv_2d(network, 256, 3, activation='relu',name='Conv3_1')
+network = conv_2d(network, 256, 3, activation='relu', strides=2,name='Conv3_2')
 network = batch_normalization(network)
 
 #conv4 32x32x256
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu',name='Conv4_1')
+network = conv_2d(network, 512, 3, activation='relu',name='Conv4_2')
 network = batch_normalization(network)
 
 #conv5 32x32x512
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu',name='Conv5_1')
+network = conv_2d(network, 512, 3, activation='relu', name='Conv5_2')
 network = batch_normalization(network)
 
 #conv6 32x32x512
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu', name='Conv6_1')
+network = conv_2d(network, 512, 3, activation='relu', name='Conv6_2')
 network = batch_normalization(network)
 
 #conv7 32x32x512
-network = conv_2d(network, 512, 3, activation='relu')
-network = conv_2d(network, 512, 3, activation='relu')
+network = conv_2d(network, 512, 3, activation='relu', name='Conv7_1')
+network = conv_2d(network, 512, 3, activation='relu', name='Conv7_2')
 network = batch_normalization(network)
 
 #conv8 32x32x512
-network = conv_2d(network, 256, 3, activation='relu')
-network = conv_2d_transpose(network, 256 , 3, [64,64,256], activation='relu', strides = 2)
+network = conv_2d(network, 256, 3, activation='relu',name='Conv8_1')
+network = conv_2d_transpose(network, 256 , 3, [64,64,256], activation='relu', strides = 2,name='Conv8_2')
 
 #conv8 64x64x256
-network = conv_2d(network, 128, 3, activation='relu')
-network = conv_2d_transpose(network, 128 , 3, [128,128,128], activation='relu', strides = 2)
+network = conv_2d(network, 128, 3, activation='relu',name='Conv9_1')
+network = conv_2d_transpose(network, 128 , 3, [128,128,128], activation='relu', strides = 2, name='Conv9_2')
 
 #conv8 128x128x128
-network = conv_2d(network, 6, 3, activation='relu')
-network = conv_2d_transpose(network, 3 , 3, [256,256,3], activation='relu', strides=2)
-loss = losses.L2(Y-network)
+network = conv_2d(network, 6, 3, activation='relu',name='Conv10_1')
+network = conv_2d_transpose(network, 3 , 3, [256,256,3], activation='relu', strides=2,name='Conv10_2')
 
 network = regression(network, optimizer='Adam',
-                     loss=loss,
+                     loss='mean_square',
                      learning_rate=0.0001)
 
 model = DNN(network, checkpoint_path='saveFiles/colorizer',
@@ -71,8 +70,7 @@ model = DNN(network, checkpoint_path='saveFiles/colorizer',
 #model = DNN(network)
 
 #model.load('Colorize')
-
-model.fit(X, Y, n_epoch=10, validation_set=0.1, shuffle=True,
+model.fit(X,Y, n_epoch=10, validation_set=0.1, shuffle=True,
           show_metric=True, batch_size=32, snapshot_step=1000,
           snapshot_epoch=False, run_id='flowers')
 
